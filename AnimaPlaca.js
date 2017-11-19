c336deb3d36d2ac098c92c96ae9419bf1087d39d
NaturesProphet <mateus.gigainfo@gmail.com>
2017-11-18 22:11:22 -0200

    Fracionamento da imagem do policial e adaptação no script

--- AnimaPlaca.js
+++ AnimaPlaca.js
@@ -28,11 +28,27 @@ function DesenhaPlaca() {
 
 //renderiza o Oficial BarBraddy no canvas
 function DesenhaPolicial() {
-    var policial = new Image();
-    policial.src = "policial/policia.png"
-    origemx = canvas.width - policial.width;
-    origemy = canvas.height - policial.height;
-    policial.onload = function () {
-        ctx.drawImage(policial, origemx, origemy);
+    var kbeca = new Image();
+    var korpo = new Image();
+    kbeca.src = "policial/Kbeca.png";
+    korpo.src = "policial/Korpo.png";
+    //vamo renderiza sapoha aqui no cantinho direito
+    
+    origemx = canvas.width - korpo.width;
+    origemy = canvas.height - korpo.height;
+    
+    korpo.onload = function () {
+        ctx.drawImage(korpo,origemx,origemy);
+    }
+    
+    var kbecax = origemx;   //ponto x inicial da cabeça
+    var kbecay = origemy - kbeca.height +1; //idem pro Y
+    
+    
+    kbeca.onload = function() {
+        ctx.drawImage(kbeca,kbecax,kbecay);
     }
 }
+
+
+
