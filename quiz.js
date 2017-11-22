dfb32574d15e3d9e4b06c05b65b8aecd0d1444aa
NaturesProphet <mateus.gigainfo@gmail.com>
2017-11-22 18:40:11 -0200

    Quiz dinamico em seu primeiro prototipo funcional

--- quiz.js
+++ quiz.js
@@ -5,25 +5,27 @@
     ele e levemente orientado a objeto, pois tentarei usar classes
     para fazer as perguntas.
     
+    Nao possui dependencias para ser carregado mas e requisito 
+    de outros scripts
+    
     autor: Mateus Garcia
     e-mail: mateus.gigainfo@gmail.com
 */
 
 //declaracao da classe Pergunta
 
-function Pergunta() {
+function Pergunta(numero) {
     //opcoes de resposta
     this.a = false;
     this.b = false;
     this.c = false;
     this.d = false;
-    
+
     //resposta correta
-    this.resposta = 'd'; //setar a resposta aqui
+    this.resposta = ''; //setar a resposta aqui
 
     //Arquivo de imagem contendo a placa a ser desenhada
     this.placa = new Image();
-    placa.src = ""; //setar o caminho d aplaca
 
 
     //Strings dinamicas (Respostas da pergunta)
@@ -33,17 +35,85 @@ function Pergunta() {
     this.r3 = ' ';
     this.r4 = ' ';
 
+    //define o fim do jogo quando acabar as perguntas
+    this.gameover = false;
+
+    /*
+    ARSENAL DE PERGUNTAS ABAIXO VIA CONSTRUTOR
+    ############################################
+    ############################################
+    */
+    switch (numero) {
+
+        case 0:
+
+            this.placa.src = "placas/VelMax.PNG";
+            this.r1 = "Velocidade Minima permitida 80KM/h";
+            this.r2 = "Velocidade Maxima permitida 80KM/h";
+            this.r3 = "Pistas em 8 e circulares a frente";
+            this.r4 = "Pedagio R$ 80,00";
+            this.resposta = 'b';
+
+            break;
+
+
+
+
+
+
+        default:
+            this.gameover = true;
+            break;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    /*
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    
+                AQUI TERMINA O ARSENAL DE PERGUNTAS
+                ABAIXO, SEGUE A CONTINUACAO DA CLASSE
+            
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    ############################################################
+    */
+
+
 
     //Strings estaticas (Estrutura HTML do formulario)
     this.l1 = "<form>";
-    this.l2 = "<input type='radio' name = 'resp' id='a'>" + this.r1 + "<br>";
-    this.l3 = "<input type='radio' name = 'resp' id='b'>" + this.r2 + "<br>";
-    this.l4 = "<input type='radio' name = 'resp' id='c'>" + this.r3 + "<br>";
-    this.l5 = "<input type='radio' name = 'resp' id='d'>" + this.r4 + "<br>";
+    this.l2 = "<input type='radio' name = 'resp' id='a'>" + this.r1 + "<br> <br>";
+    this.l3 = "<input type='radio' name = 'resp' id='b'>" + this.r2 + "<br><br>";
+    this.l4 = "<input type='radio' name = 'resp' id='c'>" + this.r3 + "<br><br>";
+    this.l5 = "<input type='radio' name = 'resp' id='d'>" + this.r4 + "<br><br>";
     this.l6 = "<br><br>";
     this.l7 = "<input type='reset' value='Clear'>";
     this.l8 = "<input type='button' value = 'OK' onclick='pergunta.ValidaResposta()'>";
     this.l9 = "</form>";
+
+
 
 }
 //implementaçao dos metodos da classe
@@ -54,12 +124,14 @@ function Pergunta() {
 */
 Pergunta.prototype.montar = function () {
     var div = document.getElementById('quiz');
-    var html = this.l1 + this.l2 + this.l3 + this.l4 + this.l5 +
-        this.l6 + this.l7 + this.l8 + this.l9;
-    div.innerHTML = html;
+    if (this.gameover == false) {
+        var html = this.l1 + this.l2 + this.l3 + this.l4 + this.l5 +
+            this.l6 + this.l7 + this.l8 + this.l9;
+        div.innerHTML = html;
+    } else {
+        div.innerHTML = "<br><hr>Fim de Jogo<hr>";
+    }
 };
-
-
 
 /*
     Este metodo valida a resposta fornecida pelo 
@@ -71,29 +143,18 @@ Pergunta.prototype.ValidaResposta = function () {
     var radio2 = document.getElementById('b');
     var radio3 = document.getElementById('c');
     var radio4 = document.getElementById('d');
-    
+
     if (radio1.checked) userchoice = 'a';
     if (radio2.checked) userchoice = 'b';
     if (radio3.checked) userchoice = 'c';
     if (radio4.checked) userchoice = 'd';
-    
+
     if (userchoice == pergunta.resposta) {
-        //rotinas pro caso do acerto
+        alert("ACERTOOOOOOOO MISERAVI");
+        NovaPergunta();
     } else {
-        
-        //rotinas pro caso do erro
+
+        alert("EROOOUUUUU");
+        NovaPergunta();
     }
 };
-
-
-
-
-
-pergunta = new Pergunta();
-pergunta.montar();
-
-function validar() {
-    console.log('1 chamada');
-    pergunta.ValidaResposta();
-    
-}
